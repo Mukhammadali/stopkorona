3131d0db1335457e22316bf918be5e4407794516
Muhammad ali <mukhammad@appliedlabs.io>
2020-03-30 12:15:22 +0500

    update manifest file

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,12 +20,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `StopKorona.uz`,
+        short_name: `StopKorona`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
