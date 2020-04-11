bbf99eb5d6e59bb29a63810791f0501a5403755f
Muhammad ali <toshboev7@gmail.com>
2020-04-11 08:42:57 +0500

    remove offline functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,12 +52,12 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/index/`, `/countries/`],
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-offline`,
+    //   options: {
+    //     precachePages: [],
+    //   },
+    // },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
