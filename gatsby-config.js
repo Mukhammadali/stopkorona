66fd03c5b1752c980864f9743a7d007035fbb0c1
Muhammad ali <toshboev7@gmail.com>
2020-04-26 13:47:12 +0500

    add offline support

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,12 +51,12 @@ module.exports = {
         icon: `src/static/images/logo.png`, // This path is relative to the root of the site.
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-offline`,
-    //   options: {
-    //     precachePages: [],
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: ['/', '/global', '/countries'],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
